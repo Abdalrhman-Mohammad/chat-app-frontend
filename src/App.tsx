9dcf6c69075cb9274a11db1881b46f7cbb79ad9f
Abdalrahman-Mohammad <abd0004@hotmail.com>
2024-09-27 15:48:37 +0300

    Add Feature: When a message is received, a blue dot will appear next to the chat. Additionally, whether I send or receive a message, the chat will move to the top of the list. All this information will be saved internally and externally.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
@@ -7,32 +7,70 @@ import MessageScreen from './screens/MessagesScreen';
 import UserInfoState from './contexts/UserInfo';
 import ChatInfoState from './contexts/ChatInfo';
 import MessagesInfoState from './contexts/MessagesInfo';
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid, Platform} from 'react-native';
 export type RootStackParamList = {
   Home: undefined;
   Chats: undefined;
-  Messages: {title: string};
+  Messages: {title: string; rerenderFlatList: () => void};
 };
 
+async function requestNotificationPermission() {
+  if (Platform.OS === 'android') {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Notification permission granted');
+      } else {
+        console.log('Notification permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+}
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus);
+  }
+}
 function App(): React.JSX.Element {
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+  });
+  useEffect(() => {
+    const unsubscribe = messaging().onTokenRefresh(token => {
+      console.log('New FCM Token:', token);
+    });
+
+    return unsubscribe;
+  }, []);
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
-      <UserInfoState>
-        <ChatInfoState>
-          <MessagesInfoState>
-            <NavigationContainer>
-              <Stack.Navigator
-                initialRouteName="Home"
-                screenOptions={{headerShown: false}}>
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Chats" component={ChatScreen} />
-                <Stack.Screen name="Messages" component={MessageScreen} />
-              </Stack.Navigator>
-            </NavigationContainer>
-          </MessagesInfoState>
-        </ChatInfoState>
-      </UserInfoState>
+    <UserInfoState>
+      <ChatInfoState>
+        <MessagesInfoState>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{headerShown: false}}>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Chats" component={ChatScreen} />
+              <Stack.Screen name="Messages" component={MessageScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </MessagesInfoState>
+      </ChatInfoState>
+    </UserInfoState>
   );
 }
-
 
 export default App;
--- src/screens/ChatsScreen/index.tsx
+++ src/screens/ChatsScreen/index.tsx
@@ -1,5 +1,5 @@
-import {FlatList, Image, Pressable, Text, TextInput, View} from 'react-native';
-import React, {useContext, useEffect, useRef} from 'react';
+import {FlatList, Image, Pressable, Text, View} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../App';
 import styles from './styles';
@@ -8,18 +8,54 @@ import {chatInfoContext} from '../../contexts/ChatInfo';
 import MainButton from '../../components/MainButton';
 import MyModal from '../../components/myModal';
 import {userInfoContext} from '../../contexts/UserInfo';
+import {socket} from '../../utils';
+import {Chat} from '../../types/Chat';
 type ChatsProps = NativeStackScreenProps<RootStackParamList, 'Chats'>;
 
 const ChatScreen = ({navigation}: ChatsProps) => {
   const userContext = useContext(userInfoContext);
   const chatsContext = useContext(chatInfoContext);
+  const [selectedID, setSelectedID] = useState<string>('');
   const {chatsInfo, modalVisible, setModalVisible} = chatsContext;
   const logout = () => {
     userContext.logout();
     chatsContext.setChatsInfo([]);
     navigation.replace('Home');
-  };
-
+  }; 
+  function rerenderFlatList() {
+    if (selectedID == '0') setSelectedID('1');
+    else setSelectedID('0');
+  }
+  function changeState(title: string, newValue: boolean) {
+    let id = -1;
+    const chats: Chat[] = chatsInfo;
+    const foundChat = chats.filter((chat: any, index: number) => {
+      if (chat.title === title) id = index;
+      return chat.title === title;
+    });
+    if (foundChat.length > 0) {
+      foundChat[0].messageRecevied = newValue;
+    }
+    if (newValue) {
+      const tmp = {title: title, messageRecevied: newValue};
+      chats.splice(id, 1);
+      chats.unshift(tmp);
+    }
+    // chatsContext.setChatsInfo(chats);
+    chatsContext.updateStoredUserInfoWhenMessageStateChanged(
+      chats,
+      userContext.userInfo.name,
+    );
+    rerenderFlatList();
+  }
+  socket.on('newMessage', data => {
+    changeState(data.title, true);
+  });
+  useEffect(() => {
+    return () => {
+      socket.off('newMessage'); // Removes the listener
+    };
+  }, []);
   return (
     <View style={styles.mainContainer}>
       <View style={styles.container}>
@@ -39,13 +75,23 @@ const ChatScreen = ({navigation}: ChatsProps) => {
       <View style={styles.list}>
         <FlatList
           data={chatsInfo}
+          extraData={selectedID}
           renderItem={({item}) => {
             return (
               <Pressable
-                onPress={() =>
-                  navigation.push('Messages', {title: item.title})
-                }>
-                <OuterChatComponent title={item.title} />
+                onPress={() => {
+                  if (item.messageRecevied) {
+                    changeState(item.title, false);
+                  }
+                  navigation.push('Messages', {
+                    title: item.title,
+                    rerenderFlatList: rerenderFlatList,
+                  });
+                }}>
+                <OuterChatComponent
+                  title={item.title}
+                  messageRecevied={item.messageRecevied}
+                />
               </Pressable>
             );
           }}
