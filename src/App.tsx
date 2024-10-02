12b4531dc066525c49fd0e04ce3ac3d5fb154492
Abdalrahman-Mohammad <abd0004@hotmail.com>
2024-10-02 10:00:38 +0300

    use NavigationContainerComponent in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import React, {useEffect} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import ChatScreen from './screens/ChatsScreen';
-import MessageScreen from './screens/MessagesScreen';
+import React from 'react';
+import NavigationContainerComponent from './navigation/NavigationContainerComponent';
 import UserInfoState from './contexts/UserInfo';
 import ChatInfoState from './contexts/ChatInfo';
 import MessagesInfoState from './contexts/MessagesInfo';
-import messaging from '@react-native-firebase/messaging';
-import {PermissionsAndroid, Platform} from 'react-native';
-export type RootStackParamList = {
-  Home: undefined;
-  Chats: undefined;
-  Messages: {title: string; rerenderFlatList: () => void};
-};
 
 function App(): React.JSX.Element {
-  const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <UserInfoState>
       <ChatInfoState>
         <MessagesInfoState>
-          <NavigationContainer>
-            <Stack.Navigator
-              initialRouteName="Home"
-              screenOptions={{headerShown: false}}>
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="Chats" component={ChatScreen} />
-              <Stack.Screen name="Messages" component={MessageScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
+          <NavigationContainerComponent />
         </MessagesInfoState>
       </ChatInfoState>
     </UserInfoState>
