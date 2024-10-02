fd807cbe68719edeb4012349543891db6a69ddbc
Abdalrahman-Mohammad <abd0004@hotmail.com>
2024-10-02 09:56:32 +0300

    add NavigationContainerComponent

---
+++ src/navigation/NavigationContainerComponent.tsx
@@ -0,0 +1,31 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import ChatScreen from '../screens/ChatsScreen';
+import MessageScreen from '../screens/MessagesScreen';
+export type RootStackParamList = {
+  Home: undefined;
+  Chats: undefined;
+  Messages: {title: string; rerenderFlatList: () => void};
+};
+const NavigationContainerComponent = () => {
+
+  const Stack = createNativeStackNavigator<RootStackParamList>();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chats" component={ChatScreen} />
+        <Stack.Screen name="Messages" component={MessageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default NavigationContainerComponent;
+
+const styles = StyleSheet.create({});
